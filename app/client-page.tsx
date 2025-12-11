38140f4f58e84e861f40831cd36b8a5a2d3d59ed
enzo-prism <215033440+enzo-prism@users.noreply.github.com>
2025-12-10 18:29:48 -0800

    Remove core offerings section from homepage

--- app/client-page.tsx
+++ app/client-page.tsx
@@ -117,7 +117,6 @@ export default function ClientPage() {
         <SegmentsGrid />
         <ReferralSection />
         <HottestContentSection />
-        <CoreOfferingsSection />
         <WebsitesFeatures />
         <LatestPostsSection />
 
